5b51b5e869e9d66ad31ca010538c5d43c6daa2f0
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 16:09:06 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -6,8 +6,7 @@ streamlit.text("Omega 3 & Blueberry")
 streamlit.text("Cake 3 & Spinach")
 streamlit.text("Hord bolied eggs")
 
-
-
+import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
