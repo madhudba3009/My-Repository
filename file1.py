6d4bd6c73d1a6ed99efcfa2dc60f5338d956bc13
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 09:20:06 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -8,4 +8,4 @@ streamlit.text("Hord bolied eggs")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streanlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
