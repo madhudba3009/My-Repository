8d302f33946d70c0b1643caa0a6a538cb387b78b
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 09:08:13 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("MY PARENT MOM HEALTH DINNER")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry")
+streamlit.text("Cake 3 & Spinach")
+streamlit.text("Hord bolied eggs")
